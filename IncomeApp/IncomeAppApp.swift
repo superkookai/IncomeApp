85bb0ce9726001df324b3407a952111297faf61c
Weerawut <weerawut@live.com>
2025-01-15 09:18:56 +0700

    Initial Commit

---
+++ IncomeApp/IncomeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IncomeAppApp.swift
+//  IncomeApp
+//
+//  Created by Weerawut Chaiyasomboon on 15/1/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct IncomeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
